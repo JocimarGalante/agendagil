f4a1f31d5f5d378806cc27eacab9c5e8af115b21
JocimarGalante <jocimarcgalante@hotmail.com>
2025-06-18 00:13:04 -0300

    Adicionado o botão reagendamento

--- agendagil-app/src/app/pages/dashboard-paciente/consultas/consultas.component.ts
+++ agendagil-app/src/app/pages/dashboard-paciente/consultas/consultas.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Consulta } from '@models/consulta.model';
 import { StatusConsulta } from '@models/status-consulta.model';
 import { ConsultaService } from './consulta.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-consultas',
@@ -11,6 +12,7 @@ import { ConsultaService } from './consulta.service';
 export class ConsultasComponent implements OnInit {
   StatusConsulta = StatusConsulta;
   consultas: Consulta[] = [];
+
   constructor(private consultaService: ConsultaService) {}
 
   ngOnInit(): void {
@@ -19,6 +21,7 @@ export class ConsultasComponent implements OnInit {
       error: (erro) => console.error('Erro ao buscar consultas', erro),
     });
   }
+
   formatarDataHora(data: string, hora: string): string {
     const dateTime = new Date(`${data}T${hora}`);
     return dateTime.toLocaleString('pt-BR', {
@@ -32,8 +35,42 @@ export class ConsultasComponent implements OnInit {
 
   cancelarConsulta(id: number) {
     const consulta = this.consultas.find((c) => c.id === id);
-    if (consulta) {
-      consulta.status = StatusConsulta.Cancelada;
-    }
+    if (!consulta) return;
+
+    Swal.fire({
+      title: 'Cancelar consulta?',
+      text: `Deseja realmente cancelar a consulta com ${consulta.medico}?`,
+      icon: 'warning',
+      showCancelButton: true,
+      confirmButtonText: '<strong>Sim</strong>',
+      cancelButtonText: 'Não',
+      buttonsStyling: true,
+      customClass: {
+        confirmButton: 'btn btn-danger swal2-confirm-custom',
+        cancelButton: 'btn btn-outline-secondary swal2-cancel-custom',
+        popup: 'swal2-border-radius',
+      },
+    }).then((result) => {
+      if (result.isConfirmed) {
+        consulta.status = StatusConsulta.Cancelada;
+
+        Swal.fire({
+          title: 'Cancelada!',
+          text: 'A consulta foi cancelada com sucesso.',
+          icon: 'success',
+          confirmButtonText: 'OK',
+          buttonsStyling: true,
+          customClass: {
+            confirmButton: 'btn btn-success swal2-confirm-custom',
+            popup: 'swal2-border-radius',
+          },
+        });
+      }
+    });
+  }
+
+  reagendarConsulta(consulta: Consulta): void {
+    console.log('Reagendando consulta:', consulta);
+    // Exemplo: this.router.navigate(['/reagendar', consulta.id]);
   }
 }
