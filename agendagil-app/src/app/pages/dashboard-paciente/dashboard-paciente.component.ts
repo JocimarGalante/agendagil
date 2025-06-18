ac9559738c7fbdea98617c913c40fc0e65b0aa12
JocimarGalante <jocimarcgalante@hotmail.com>
2025-06-17 23:33:06 -0300

    Ajuste na responsividade

--- agendagil-app/src/app/pages/dashboard-paciente/dashboard-paciente.component.ts
+++ agendagil-app/src/app/pages/dashboard-paciente/dashboard-paciente.component.ts
@@ -10,11 +10,17 @@ import { Usuario } from '@models/usuario.model';
 export class DashboardPacienteComponent implements OnInit {
   usuario: Usuario | null = null;
   isCollapsed = false;
+  isMobile = false;
 
   constructor(private authService: AuthService) {}
 
   ngOnInit(): void {
     this.usuario = this.authService.getUsuarioLogado();
+    this.isMobile = window.innerWidth <= 768;
+    window.addEventListener('resize', () => {
+      this.isMobile = window.innerWidth <= 768;
+      if (this.isMobile) this.isCollapsed = true;
+    });
   }
 
   toggleSidebar() {
