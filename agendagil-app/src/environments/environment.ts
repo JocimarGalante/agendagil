1ed70ff45f66be5d67b9f68b7ad4e7aaacb947c5
JocimarGalante <jocimarcgalante@hotmail.com>
2025-11-18 17:39:57 -0300

    mudança API

--- agendagil-app/src/environments/environment.ts
+++ agendagil-app/src/environments/environment.ts
@@ -2,7 +2,7 @@
 export const environment = {
   production: false,
   supabase: {
-    url: 'https://wnvsgcmbecmqwfauzkni.supabase.co',
-    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6IndudnNnY21iZWNtcXdmYXV6a25pIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjMzODQyNDAsImV4cCI6MjA3ODk2MDI0MH0.eaRWY-VaoHtKHxkKgmeafGvYL95oU6VXAoYpHh--I_c'
+    url: 'https://vnwqxbwizkyygrxjktxy.supabase.co',
+    key: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZud3F4Yndpemt5eWdyeGprdHh5Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjM0NTcyNTUsImV4cCI6MjA3OTAzMzI1NX0.W4C-0N6ykFDARcmNTepxWE9uWr_AlsQ3KJztFR97a5U'
   }
 };
